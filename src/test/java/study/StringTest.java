1ba152fd2a193d9e68abbb1e7feae9ba517661f1
Jivebreaddev <jivebreaddev@gmail.com>
2023-05-18 21:49:36 +0900

    playground first requirement: write splitArray, splitSingle tests

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,17 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+    @Test
+    void splitArray() {
+        String actual = "1,2";
+        String[] result = actual.split(",");
+        assertThat(result).contains("1","2");
+    }
+    @Test
+    void splitSingle() {
+        String actual = "1";
+        String[] result = actual.split(",");
+        assertThat(result).containsExactly("1");
+    }
+
 }
