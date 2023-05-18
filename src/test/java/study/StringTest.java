9aef63d9b86c01b8ede9748538e2f36424170f6e
Jivebreaddev <jivebreaddev@gmail.com>
2023-05-18 22:05:32 +0900

    tests for String methods

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 public class StringTest {
     @Test
@@ -22,5 +24,34 @@ public class StringTest {
         String[] result = actual.split(",");
         assertThat(result).containsExactly("1");
     }
+    @Test
+    void splitSingleWithParentheses() {
+        String actual = "(1,2)";
+        actual = actual.replace("(", "");
+        actual = actual.replace(")", "");
+        String[] result = actual.split(",");
+        assertThat(result).contains("1","2");
+    }
 
+    @Test
+    @DisplayName("charAt 작동 테스트")
+    void charAt() {
+        String actual = "1,2";
+
+        char result = actual.charAt(0);
+        assertThat(result).isEqualTo('1');
+    }
+    @Test
+    @DisplayName("charAt Exception 핸들링하는것 확인하기")
+    void charAtException() {
+
+        // given some preconditions
+        String actual = "1,2";
+
+        // when
+        Throwable thrown = catchThrowable(() -> { actual.charAt(5); });
+
+        // then
+        assertThat(thrown).isInstanceOf(Exception.class);
+    }
 }
