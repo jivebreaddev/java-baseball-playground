33764854adf62d1a9d238dba1031b16ea70a6df0
Jivebreaddev <jivebreaddev@gmail.com>
2023-05-20 09:11:30 +0900

    Calculator Tests and script finished

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,31 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+    private Scanner scanner;
+
+    public void calculate(){
+        scanner = new Scanner(System.in);
+        String inputs = scanner.nextLine();
+        String[] values = inputs.split(" ");
+        // input processing 먼저 숫자랑 operators 이랑 나눠서 illegal 이면 exception 던지는게 맞음
+        int acc = Integer.parseInt(values[0]);
+
+        for(int i = 1; i<values.length; i+=2){
+            if (values[i].equals("+")){
+                acc += Integer.parseInt(values[i+1]);
+            }else if (values[i].equals("-")){
+                acc -= Integer.parseInt(values[i+1]);
+            }else if (values[i].equals("*")){
+                acc *= Integer.parseInt(values[i+1]);
+            }else if (values[i].equals("%")){
+                acc /= Integer.parseInt(values[i+1]);
+            }else{
+                throw new IllegalArgumentException("Please check arguments");
+            }
+
+        }
+        System.out.print(acc);
+    }
+}
---
+++ src/test/java/study/CalculatorTest.java
@@ -0,0 +1,61 @@
+package study;
+
+import calculator.Calculator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+// calculator는 Scanner 로 인풋을 받는데
+// test를 할때는 input 값이 정해져 있지 않다.
+// 해당을 input stream 종류를 바꿔서 만들 수 있다.
+public class CalculatorTest {
+    private Calculator calculator;
+    private OutputStream out;
+
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+        out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+    }
+
+    @Test
+    @DisplayName("정상적인 input일 때, calculate 하기")
+    void calculateTest(){
+        //given
+        String input = "1 + 2 + 3";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        //when
+        calculator.calculate();
+
+        //then
+        assertThat("6").isEqualTo(out.toString());
+    }
+
+    @Test
+    @DisplayName("systemin Exception 포맷에 맞지 않는 string 주어 졌을때")
+    void wrongArgumentsTest(){
+        // given
+        String input = "1 + 2k + 3";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+
+        // when
+        Throwable thrown = catchThrowable(() -> {
+            calculator.calculate();
+            out = new ByteArrayOutputStream();
+        });
+
+        // then
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
