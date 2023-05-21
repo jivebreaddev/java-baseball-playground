e461ed718ced9845fdd9153a86b4d19cd705c928
Jivebreaddev <jivebreaddev@gmail.com>
2023-05-22 06:38:24 +0900

    test: (baseball) add six tests given inputs from the user

--- src/test/java/study/BaseBallTest.java
+++ src/test/java/study/BaseBallTest.java
@@ -1,4 +1,62 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+//import assertThat
+//import catchThrowable
+//given
+//when
+//then
 public class BaseBallTest {
+
+    @Test
+    @DisplayName("systemin Exception 포맷에 맞지 않는 string 주어졌을때")
+    public void wrongInput(){
+        //given
+        //when
+        //then
+        return;
+    }
+    @Test
+    @DisplayName("인풋이 strikes의 경우를 포함 할때")
+    public void strikeTest(){
+        //given
+        //when
+        //then
+        return;
+    }
+    @Test
+    @DisplayName("인풋이 ball의 경우를 포함할 때")
+    public void ballTest(){
+        //given
+        //when
+        //then
+        return;
+    }
+    @Test
+    @DisplayName("인풋이 정답을 받았을 때")
+    public void correctAnswer(){
+        //given
+        //when
+        //then
+        return;
+    }
+
+    @Test
+    @DisplayName("게임이 끝나고 게임이 다시 시작될때")
+    public void rematchGame(){
+        //given
+        //when
+        //then
+        // return;
+    }
+    @Test
+    @DisplayName("게임이 끝나고 게임을 끝냈을 때")
+    public void endGame(){
+        //given
+        //when
+        //then
+        return;
+    }
 }
