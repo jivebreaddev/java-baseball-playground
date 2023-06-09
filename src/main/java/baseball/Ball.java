258cb24364551529925f9159c2e04d5bb50f6685
Jivebreaddev <jivebreaddev@gmail.com>
2023-06-09 21:01:42 +0900

    feat(Ball,Test,BallStatus): Constructor Illegal Argument Exceptions
    Ball tests when 1~9 ball numbers
               when index out of range

--- src/main/java/baseball/Ball.java
+++ src/main/java/baseball/Ball.java
@@ -4,12 +4,22 @@ import lombok.Data;
 
 @Data
 public class Ball {
+    public static final int NUMBER_MIN = 1;
+    public static final int NUMBER_MAX = 9;
+    public static final int POSITION_MIN = 0;
+    public static final int POSITION_MAX = 2;
     public int index;
     public int ballNumber;
     public Ball(int index, int ballNumber) {
+
+        if(ballNumber > NUMBER_MAX || ballNumber < NUMBER_MIN){
+            throw new IllegalArgumentException(String.format("ballNumber should be between %d and %d", NUMBER_MIN, NUMBER_MAX));
+        }
+        if(index > POSITION_MAX || index < POSITION_MIN){
+            throw new IllegalArgumentException(String.format("index should be between %d and %d", POSITION_MIN, POSITION_MAX));
+        }
         this.index = index;
         this.ballNumber = ballNumber;
-
     }
     public BallStatus play(Ball ball){
         if (this.equals(ball)){
--- src/test/java/baseball/BallTest.java
+++ src/test/java/baseball/BallTest.java
@@ -4,7 +4,31 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
 public class BallTest {
+
+
+    @Test
+    @DisplayName("1~9가 아닌 BallNumber 받았을 때 생성자 오류생성 IllegalArgumentException")
+    void throwErrorWhenNot19(){
+        //given
+
+        //when
+        Throwable thrown = catchThrowable(() -> new Ball(1,10));
+        //then
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    @DisplayName("0~2가 아닌 인덱스를 받았을 때 생성자 오류생성 IllegalArgumentException")
+    void throwErrorWhenIndex(){
+        //given
+
+        //when
+        Throwable thrown = catchThrowable(() -> new Ball(3,0));
+        //then
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
+    }
     @Test
     @DisplayName("볼을 받았을때")
     void playBall(){
